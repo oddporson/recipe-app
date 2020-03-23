07fe70d2dd39839fa03c8dfe1922a198db1eae08
Porson <porsonlee@gmail.com>
2020-03-23 02:31:21 -0700

    implmented getSearch function so when user clicked search after confirming what food they pick, it will fetch the data of the food they pick.

--- src/App.js
+++ src/App.js
@@ -9,26 +9,32 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('chicken');
 
   useEffect( () => {
-    // console.log('Effect has been run');
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    // console.log(data.hits);
   }
 
   const updateSearch = e => {
     setSearch(e.target.value)
+    // console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
   }
 
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">
           Search
@@ -36,12 +42,12 @@ const App = () => {
       </form>
       {recipes.map(recipe => (
         <Recipe 
-          key={recipe.recipe.label} // to remove the console log err "Each child in a list should have a unique "key" prop."
+          key={recipe.recipe.label}
           title={recipe.recipe.label} 
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
         />
-      ))};
+      ))}
     </div>
   );
 };
