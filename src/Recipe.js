31a6293c1bed67626171a05f05a71a9f689824e8
Porson <porsonlee@gmail.com>
2020-03-23 02:51:22 -0700

    the style.recipe was meant to apply as a div of the whole entire thing and not just the title

--- src/Recipe.js
+++ src/Recipe.js
@@ -3,8 +3,8 @@ import style from './recipe.module.css';
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return(
-    <div>
-        <h1 className={style.recipe}>{title}</h1>
+    <div className={style.recipe}>
+        <h1>{title}</h1>
         <ol>
           {ingredients.map(ingredient => (
             <li>{ingredient.text}</li>
