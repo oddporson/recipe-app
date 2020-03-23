a77cb37147145d6ba77f5af69c38b1c7e1417a73
Porson <porsonlee@gmail.com>
2020-03-23 01:30:06 -0700

    created a search form and playing around with onClick on h1 tag to understand the useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
 
-    const APP_ID = 'a0c6a7e5';
-    const APP_KEY = 'f31fa5c1d11aeca6903ac6564af282e6';
+  const APP_ID = 'a0c6a7e5';
+  const APP_KEY = 'f31fa5c1d11aeca6903ac6564af282e6';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-    const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <h1 onClick={()=> setCounter(counter+1)}>{counter}</h1>
     </div>
-  )
+    )
 }
 
 export default App;
