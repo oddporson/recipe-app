1136af4ab7ad8266a74dc9e80bf8f8d14105b080
Porson <porsonlee@gmail.com>
2020-03-23 02:49:42 -0700

    learned about module.css

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 const Recipe = ({ title, calories, image, ingredients }) => {
   return(
     <div>
-        <h1>{title}</h1>
+        <h1 className={style.recipe}>{title}</h1>
         <ol>
           {ingredients.map(ingredient => (
             <li>{ingredient.text}</li>
