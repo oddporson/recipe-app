f280d775b9db17b3d8bdfcdd908de475e22be77c
Porson <porsonlee@gmail.com>
2020-03-23 02:00:53 -0700

    created a recipe component and passed down api data when it gets rendered in App.js and then it passes down into Recipe component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const App = () => {
@@ -6,18 +7,18 @@ const App = () => {
   const APP_ID = 'a0c6a7e5';
   const APP_KEY = 'f31fa5c1d11aeca6903ac6564af282e6';
 
+  const [recipes, setRecipes] = useState([]);
 
   useEffect( () => {
     // console.log('Effect has been run');
     getRecipes();
-
   }, []);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
-
+    setRecipes(data.hits);
+    console.log(data.hits);
   }
 
   return (
@@ -28,8 +29,15 @@ const App = () => {
           Search
         </button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+          title={recipe.recipe.label} 
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))};
     </div>
-    )
-}
+  );
+};
 
 export default App;
